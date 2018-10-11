767c9d27fb4e831bc84c74cf26103a1893d6c283
BrandonJesws <jesusw52@gmail.com>
2018-10-10 23:15:35 -0500

    JQuery UI mover objetos

--- AprendiendoJquery/js/06-interface.js
+++ AprendiendoJquery/js/06-interface.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+    //Mover elemento por la pagina
+    $(".elemento").draggable();
+    //Redimencionar
+    $(".elemento").resizable();
+    //Seleccionar y ordenar elementos 
+    //$(".lista-seleccionable").selectable();
+    $(".lista-seleccionable").sortable({
+        update: function(event, ui){
+            console.log("Cambio la lista");
+        }
+    });
 
+    //Drop
+    $("#elemento-movil").draggable()
+    $("#area").droppable({
+        drop: function(){
+            console.log("has soltado algo dentro del area");
+        }
+    });
 });
