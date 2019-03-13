1e4311b06a4d31186173aede9ef480359c0852a5
BrandonJesws <jesusw52@gmail.com>
2019-03-12 23:18:57 -0600

    Agregar registris a base de datos Mongo

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ var app = express();
 
 // cargar archivos de rutas
 
+var project_routes = require('./rutes/project'); //cargamos el archivo de rutas
+
 // middlewares: son capas que se ejecutan antes de ejecutar el resultado de la ejecucion
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json()); //Combierte todo lo que llega a JSON
@@ -14,6 +16,14 @@ app.use(bodyParser.json()); //Combierte todo lo que llega a JSON
 // CORS
 
 // rutas
+
+app.use('/', project_routes);
+
+
+
+
+
+/*
 app.get('/', (req, res) =>{
 	res.status(200).send(
 		"<h1>Pagina de inicio</h1>"
@@ -26,6 +36,7 @@ app.post('/test', (req, res) =>{
 	res.status(200).send({
 		message: "Hola Mundo desde mi API de NodeJS"
 	});
-});
+});*/
+
 // exportar
 module.exports = app;
---
+++ backend/controllers/project.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var Project = require('../models/project');
+
+var controller = {
+	home: function(req, res){
+		return res.status(200).send({
+			message: 'Soy la home'
+		});
+	},
+	test: function(req, res){
+		return res.status(200).send({
+			message: "Soy el metodo test del controlador de project"
+		});
+	},
+
+	saveProject: function(req, res){
+		var project = new Project();
+
+		var params = req.body;
+
+		project.name = params.name;
+		project.description = params.description;
+		project.category = params.category;
+		project.year = params.year;
+		project.langs = params.langs;
+		project.image = null;
+
+		//Guardar en base de datos
+		project.save((err, projectStored) =>{
+			if(err) return res.status(500).send({message: 'Error al guardar.'});
+
+			if(!projectStored) return res.status(404).send({message: 'No se pudo gusradar el proyecto.'});
+
+			return res.status(200).send({project: projectStored});
+		});
+
+	}
+};
+
+module.exports = controller;
